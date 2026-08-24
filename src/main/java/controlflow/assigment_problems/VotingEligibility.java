468b62d1bd1faaca31f86103741d2e0c2f6e9026
HAASINDHYA <hs1207@srmist.edu.in>
2026-08-24 23:41:23 +0530

    Add VotingEligibility program

---
+++ src/main/java/controlflow/assigment_problems/VotingEligibility.java
@@ -0,0 +1,25 @@
+package controlflow.assigment_problems;
+
+import java.util.Scanner;
+
+public class VotingEligibility {
+
+    public static void checkVotingEligibility(int age) {
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter student's age: ");
+        int age = sc.nextInt();
+
+        checkVotingEligibility(age);
+
+        sc.close();
+    }
+}
